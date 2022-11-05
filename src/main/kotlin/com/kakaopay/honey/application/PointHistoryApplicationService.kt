a1342de42437155c3fc59bd80f4effd3171076b1
dgahn <dgahn@icloud.com>
2022-11-05 16:41:27 +0900

    feat: 포인트 내역 조회시 존재하지 않는 멤버십 코드에 대해서 요청하는 경우 에러 발생하도록 로직 추가

--- src/main/kotlin/com/kakaopay/honey/application/PointHistoryApplicationService.kt
+++ src/main/kotlin/com/kakaopay/honey/application/PointHistoryApplicationService.kt
@@ -1,7 +1,9 @@
 package com.kakaopay.honey.application
 
+import com.kakaopay.honey.domain.MembershipJpaRepository
 import com.kakaopay.honey.domain.PointHistory
 import com.kakaopay.honey.domain.PointHistoryJpaRepository
+import com.kakaopay.honey.exception.HoneyNotFoundException
 import com.kakaopay.honey.util.toInstant
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -9,10 +11,13 @@ import java.time.LocalDate
 
 @Service
 class PointHistoryApplicationService(
-    private val pointHistoryJpaRepository: PointHistoryJpaRepository
+    private val pointHistoryJpaRepository: PointHistoryJpaRepository,
+    private val membershipJpaRepository: MembershipJpaRepository
 ) {
     @Transactional(readOnly = true)
     fun searchHistories(membershipCode: String, startAt: LocalDate, endAt: LocalDate): List<PointHistory> {
+        membershipJpaRepository.findByCode(membershipCode)
+            ?: throw HoneyNotFoundException("등록되지 않은 멤버십 코드입니다. (membershipCode: $membershipCode)")
         return pointHistoryJpaRepository.searchHistories(startAt.toInstant(), endAt.toInstant(), membershipCode)
     }
 }
--- src/test/kotlin/com/kakaopay/honey/application/PointHistoryApplicationServiceTest.kt
+++ src/test/kotlin/com/kakaopay/honey/application/PointHistoryApplicationServiceTest.kt
@@ -1,8 +1,12 @@
 package com.kakaopay.honey.application
 
+import com.kakaopay.honey.domain.MembershipJpaRepository
 import com.kakaopay.honey.domain.PointHistoryJpaRepository
+import com.kakaopay.honey.exception.HoneyNotFoundException
+import com.kakaopay.honey.fixture.MembershipFixture
 import com.kakaopay.honey.fixture.PointHistoryFixture
 import com.ninjasquad.springmockk.MockkBean
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import org.junit.jupiter.api.Test
@@ -20,12 +24,26 @@ class PointHistoryApplicationServiceTest(
     @MockkBean
     lateinit var pointHistoryJpaRepository: PointHistoryJpaRepository
 
+    @MockkBean
+    lateinit var membershipJpaRepository: MembershipJpaRepository
+
     @Test
     fun `포인트_내역을_조회할_수_있다`() {
         val expected = PointHistoryFixture.getPointHistories()
         val membershipCode = expected.first().membershipCode
         every { pointHistoryJpaRepository.searchHistories(any(), any(), any()) } returns expected
+        every { membershipJpaRepository.findByCode(any()) } returns MembershipFixture.getMembership()
         val actual = pointHistoryApplicationService.searchHistories(membershipCode, LocalDate.now(), LocalDate.now())
         actual shouldBe expected
+    }
+
+    @Test
+    fun `등록되지_않은_멤버십_코드로_포인트_내역을_조회하면_HoneyNotFoundException이_발생한다`() {
+        val expected = PointHistoryFixture.getPointHistories()
+        val membershipCode = expected.first().membershipCode
+        every { membershipJpaRepository.findByCode(any()) } returns null
+        shouldThrow<HoneyNotFoundException> {
+            pointHistoryApplicationService.searchHistories(membershipCode, LocalDate.now(), LocalDate.now())
+        }
     }
 }
